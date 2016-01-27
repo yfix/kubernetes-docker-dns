6915bb6ddf58e3f4e4e101bc9182bdedf2b3bad7
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-01-27 09:06:29 +0200

    up

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 export DNS_REPLICAS=1
 export DNS_DOMAIN=cluster.local
 export DNS_SERVER_IP=10.0.0.10
-export KUBE_VERSION=v1.1.2
-export KUBE_SERVER=http:\\/\\/192.168.99.100:8080
+export KUBE_VERSION=v1.1.4
+#export KUBE_SERVER=http:\\/\\/192.168.99.100:8080
+export KUBE_SERVER=http:\\/\\/127.0.0.1:8080
--- stop_remove.sh
+++ stop_remove.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+
+k8s=$(docker ps -a | egrep "(k8s|gcr.io)" | cut -d " " -f1)
+docker stop $k8s
+docker rm $k8s
+
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
